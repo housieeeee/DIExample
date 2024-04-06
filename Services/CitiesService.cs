219663ff08764f57a31ce2a901edb70bf0fd1fbf
house <piyushkumarsometing@gmail.com>
2024-04-07 03:53:17 +0530

    Add project files.

---
+++ Services/CitiesService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class CitiesService
+    {
+        private List<string> _cities;
+        public CitiesService()
+        {
+            _cities = new List<string>()
+            {
+            "London",
+            "Paris",
+            "New York",
+            "Tokyo",
+            "Rome"
+            };
+        }
+        public List<string> GetCities()
+        {
+            return _cities;
+        }
+    } 
+}
